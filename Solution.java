1e27a30f8c33f70d0d5b29f7231d2a21a3b9d4d5
saigeetha11 <71434879+saigeetha11@users.noreply.github.com>
2021-06-30 00:26:02 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,17 @@
+public class Solution
+ {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,m,n;
+        n=sc.nextInt();
+        for(i=1;i<=10;i++)
+        {
+            m=n*i;
+          System.out.println(n + " x "+i+" = "+m);
+        }
+        
+        
+    }
+    
+}
