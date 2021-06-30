0bace2993942c76dbeef64e98b4b3f5ff31eaad0
saigeetha11 <71434879+saigeetha11@users.noreply.github.com>
2021-06-30 21:59:01 +0530

    Add files via upload

---
+++ Replacenumber.java
@@ -0,0 +1,55 @@
+import java.io.*;
+
+import java.util.*;
+
+
+public class Replacenumber
+ 
+{
+    
+public static void main(String[] args)
+  
+  {
+        
+Scanner sc = new Scanner(System.in);
+        
+       
+ int n, x, y, s = 0, c = 1, r = 0;
+
+       
+ System.out.println("Enter a number");
+    
+ n = sc.nextInt();
+       
+ System.out.println("Enter x ");
+
+ x = sc.nextInt();
+ 
+ System.out.println("Enter y ");
+       
+ y = sc.nextInt();
+
+      
+ while(n != 0){
+            
+ r = n%10;
+            
+if(r == x)
+{
+               
+ r = y;
+         
+}           
+ s = r * c + s;
+           
+ c = c* 10;
+           
+ n = n/10;
+      
+}
+
+       
+ System.out.print("Replaced number= " +s);
+    
+}
+}
