e69f17c98ce3643590131b98b8f4d0f9870760c7
saigeetha11 <71434879+saigeetha11@users.noreply.github.com>
2021-07-01 21:14:20 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Pattern1
+{
+public static void main(String args[ ])
+{
+int i,j,n=5;
+for(i=n-1;i>=0;i--)
+{
+for(j=0;j<=i;j++)
+{
+System.out.print("*");
+}
+System.out.println(" ");
+}
+}
+}
