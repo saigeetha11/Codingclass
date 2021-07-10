5bc8379879f8dd644fff9b4c6a73eeb4ca9e44e5
saigeetha11 <71434879+saigeetha11@users.noreply.github.com>
2021-07-10 22:27:12 +0530

    Add files via upload

---
+++ Lowercase.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Lowercase
+{
+  public static void main(String args[ ])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the String: ");
+    String str=sc.nextLine( );
+    String str1=str.toLowerCase( );
+    System.out.println("Lowercase String is: "str1);
+   }
+}
+-----------------------------------------------------------------------------
+OUTPUT:
+Enter the String:
+CoDinG ClAsS
+Lowercase String is:
+coding class
