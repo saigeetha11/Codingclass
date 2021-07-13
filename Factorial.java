16650afc6168580bf41e3590eebf3e462cd7e9c9
saigeetha11 <71434879+saigeetha11@users.noreply.github.com>
2021-07-13 23:39:29 +0530

    Add files via upload

--- Factorial.java
+++ Factorial.java
@@ -1,28 +1,36 @@
 import java.util.*;
+class Facto
+{
+public void fact( )
+{
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter the n value: ");
+   int n=sc.nextInt( );
+   int fac=1;  
+   while(n>0)
+   {
+     fac=fac*n;
+     n--;
+   }
+   System.out.println("The factorial of a given number is: ");
+   System.out.print(fac);
+}
+public void display( )
+{
+fact( );
+}
 public class Factorial
 {
   public static void main(String args[ ])
   {
-   Scanner sc=new Scanner(System.in);
-   System.out.println("Enter the n value: ");
-   int n=sc.nextInt( );
-   System.out.print("The factorial of a given number is: ");
-   facto(n);
+   Facto f=new Facto( );
+   f.display( );
   }
-static void facto(int n)
-{
-   int fact=1;  
-   while(n>0)
-   {
-     fact=fact*n;
-     n--;
-   }
-  System.out.print(fact);
 }
 }
 
---------------------------------------------------------------------------------------------
+/*--------------------------------------------------------------------------------------------
 OUTPUT:
 Enter tha n value:
 10
-The factorial of a given number is: 3628800
+The factorial of a given number is: 3628800*/
